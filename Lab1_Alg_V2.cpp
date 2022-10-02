dafff5a4ca246de79b89b0196b4f9637656fb791
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2022-10-02 19:23:12 +0300

    Update Lab1_Alg_V2.cpp

--- Lab1_Alg_V2.cpp
+++ Lab1_Alg_V2.cpp
@@ -3,20 +3,6 @@
 #include <iostream>
 #include <Windows.h>
 
-std::int_fast64_t powint(int base, int exp)
-{
-    std::int_fast64_t result{ 1 };
-    while (exp)
-    {
-        if (exp & 1)
-            result *= base;
-        exp >>= 1;
-        base *= base;
-    }
-
-    return result;
-}
-
 int Razm_BP(int test, int p)
 {
     int a;
